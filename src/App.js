a475e4e597a407d5f44b16a6ff4662f276017323
webdev-faizan <webdev.faizanali@gmail.com>
2024-04-20 16:38:50 -0400

    change name

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          faizan
-          
+          faizan ali
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
