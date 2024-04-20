4d248186351db75473426aff41c583149ee8e5b0
webdev-faizan <webdev.faizanali@gmail.com>
2024-04-20 17:20:10 -0400

    update path

--- .scripts/depoly.sh
+++ .scripts/depoly.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
-sudo -i echo "Development started..."
-sudo -i git stash
-sudo -i git pull origin master
-sudo -i echo "New changes to the server !"
-sudo -i echo "Installing Dependencies..."
-sudo -i npm install --yes
-sudo -i echo "Creating production build"
-sudo -i npm run build
-sudo -i echo "Depolyment Finished!"
+echo "Development started..."
+git pull origin master
+echo "New changes to the server !"
+echo "Installing Dependencies..."
+npm install --yes
+echo "Creating production build"
+npm run build
+echo "Depolyment Finished!"
