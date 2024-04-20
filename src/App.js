36ac7ef5020a5037f24cd2cfb5a2df819ecc9b67
webdev-faizan <webdev.faizanali@gmail.com>
2024-04-20 16:24:31 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          faizan
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
