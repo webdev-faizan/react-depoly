b76b6913337116462951af483da6d9a110bfea3d
webdev-faizan <webdev.faizanali@gmail.com>
2024-04-20 16:54:32 -0400

    repush

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          faizan ali
+          faizan ali 
+          
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
